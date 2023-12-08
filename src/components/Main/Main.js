ae5fa50fafb37ccba67cb88978f426ffcf47a5e3
unknown <vpaist@studio-tg.ru>
2023-12-08 17:29:31 +0300

    test

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-confusing-arrow */
 import { useSelector } from 'react-redux';
+import { isMobile, isDesktop } from 'react-device-detect';
 import SearchForm from '../SearchForm/SearchForm';
 import CardsList from '../CardsList/CardsList';
 import ShowMoreButton from '../ShowMoreButton/ShowMoreButton';
@@ -7,22 +9,32 @@ import Preloader from '../Preloader/Preloader';
 
 export default function Main() {
   const filterStatus = useSelector((state) => state.filter.filter.status);
-  const cards = useSelector((state) => (filterStatus
-    ? state.cards.cards.filter((item) => item.liked_by_user === true)
-    : state.cards.cards));
+  const cards = useSelector((state) =>
+    filterStatus ? state.cards.cards.filter((item) => item.liked_by_user === true) : state.cards.cards);
   const isLoaded = useSelector((state) => state.cards.loader.cardsLoader);
 
   return (
     <main className="main">
       <SearchForm />
       <FilterCheckbox />
-      {!isLoaded ? (<Preloader />)
-        : cards.length !== 0 ? (
-          <>
-            <CardsList cards={cards} />
-            <ShowMoreButton />
-          </>
-        ) : (<span className="main__span">Oops! There is nothing here..yet</span>)}
+      <div>
+        {isMobile ? (
+          <p>Устройство является мобильным</p>
+        ) : (
+          <p>Устройство является десктопом</p>
+        )}
+        {isDesktop ? <p>Режим десктопа</p> : <p>Режим мобильного устройства</p>}
+      </div>
+      {!isLoaded ? (
+        <Preloader />
+      ) : cards.length !== 0 ? (
+        <>
+          <CardsList cards={cards} />
+          <ShowMoreButton />
+        </>
+      ) : (
+        <span className="main__span">Oops! There is nothing here..yet</span>
+      )}
     </main>
   );
 }
